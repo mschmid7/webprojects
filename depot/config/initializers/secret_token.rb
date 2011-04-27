4ad97791d3a8db1d3c9b9a6426a16d77e6170ae5
mschmid7 <Info@moboon.com>
2011-04-27 22:20:06 +0200

    upolad

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '5af1220e435d7dccbee3810f537451e125c438c9c9cf00c34cb782993307ea29515c452343dc34bad28b3f574ed627df2f750f5d53963f45cbe6a2855d0d1cdf'
